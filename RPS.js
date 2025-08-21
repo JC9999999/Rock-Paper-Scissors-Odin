8ad5cb97767cbdc781b6fab27312b162b7fe3650
JC9999999 <robotic32100@gmail.com>
2025-08-20 21:17:32 -0400

    Get functions uses integer as input so an integer is returned. Play rounds function showed whether the computer or human won the round. Play game function calls playround function and has the get functions stored in variables and uses those as the playround parameters. There is also a helper function inside in the Play game function that converts the user input back to string so that it displays the actual rock,paper,scissor choices. This is all in a while loop.

--- RPS.js
+++ RPS.js
@@ -1,39 +1,91 @@
+
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
 
-    let text = "";
-
     if(choice == 0){
-        text = "Computer chooses Rock";
+        return 0;
     }
-    if(choice == 1){
-        text = "Computer chooses Paper";
+    else if(choice == 1){
+        return 1;
     }
-    if (choice == 2){
-        text = "Computer chooses Scissors";
+    else if (choice == 2){
+        return 2;
     }
-    return text;
 }
 
 function getHumanChoice(){
-    let userInput = prompt("0 = Rock, 1 = Paper, 2 = Scissors", "Choose an option").toLowerCase();
-
-    let text = "";
-
-    if(userInput == 0){
-        text = "You have chosen Rock";
+    let input = prompt("0 = Rock, 1 = Paper, 2 = Scissors", "Choose an option");
+    
+    if(input == 0){
+        return 0;
     }
-    else if(userInput == 1){
-        text = "You have chosen Paper";
+    else if(input == 1){
+        return 1;
     }
-    else if(userInput == 2){
-        text = "You have chosen Scissors";
-    }
-    else{
-        text = "Choose a valid option";
-    }
-    return text;
+    else if(input == 2){
+        return 2;
+    }    
 }
+
+function playRounds(humanChoice, computerChoice){
+ 
+    // 0 = rock, 1 = paper, 2 = scissor
+    if(humanChoice === 0 && computerChoice === 2 || humanChoice === 1 && computerChoice === 0 || humanChoice === 2 && computerChoice === 1){
+        return "You win";
+    }
+    else if(humanChoice === 0 && computerChoice === 1 || humanChoice === 1 && computerChoice === 2 || humanChoice === 2 && computerChoice === 0){
+        return "You lose";
+    }
+    else if(humanChoice === computerChoice){
+        return "It's a Tie";
+    }
+}
+
+function playGame(){
+    //code for prompting user input for the amount of rounds user wants
+    // numRounds = user input
+    //roundsPlayed = rounds counter
+    let numRounds = prompt("Enter the number of rounds that you want to play", "");
+    let roundsPlayed = 0;
+    while(roundsPlayed<numRounds){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRounds(humanChoice, computerChoice);
+
+        let humanText = "";
+        if(humanChoice === 0){
+            humanText = "Rock";
+        }
+        if (humanChoice === 1){
+            humanText = "Paper";
+        }
+        if (humanChoice === 2){
+            humanText = "Scissors";
+        }
+        let computerText = "";
+        if(computerChoice === 0){
+            computerText = "Rock";
+        }
+        if (computerChoice === 1){
+            computerText = "Paper";
+        }
+        if (computerChoice === 2){
+            computerText = "Scissors";
+        }
+        console.log("You have chosen" + humanText);
+        console.log("Computer has chosen" + computerText);
+        console.log(result);
+
+        roundsPlayed++;
+    }
+
+}
+
+playGame();
+
+
+
 
 
 
